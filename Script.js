a98fa0ed63510376a4acd89f8f1ebbb83dfdf560
Jailson Paulino de Lima <91923325+Jailsonpl@users.noreply.github.com>
2021-12-14 13:43:51 -0300

    Adicionado operações

--- Script.js
+++ Script.js
@@ -1,11 +1,32 @@
-function calcular(){
+let escolha=document.querySelector("select");
+escolha.addEventListener('change',operacao);
 
-			var n = Number(document.getElementById("numero").value);
+function operacao(){
+			let oper=escolha.value;
+			var num = Number(document.getElementById("numero").value);
 			var res = document.getElementById("resultado");
 			var contador = '';
 
-			for(var i=0;i<=10;i++){
-				contador += `${n} * ${i} = ${n*i} </br>`;
-				res.innerHTML=contador;			
-			}	
+			if(num==""){
+				res.innerHTML="Digite um numero"
+			} else{	
+				if(oper=="Soma"){
+				for(var i=0;i<=10;i++){
+					contador += `${num} + ${i} = ${num+i} </br>`;				
+					}		
+				} else if(oper=="Subtração"){
+				for(var i=0;i<=10;i++){
+					contador += `${num} - ${i} = ${num-i} </br>`;				
+					}		
+				} else if(oper=="Multiplicação"){
+				for(var i=0;i<=10;i++){
+					contador += `${num} * ${i} = ${num*i} </br>`;				
+					}		
+				} else if(oper=="Divisão"){
+				for(var i=1;i<=10;i++){
+					contador += `${num} / ${i} = ${(num/i).toFixed(2)} </br>`;				
+					}		
+				}
+			res.innerHTML=contador;	
 		}
+	}
